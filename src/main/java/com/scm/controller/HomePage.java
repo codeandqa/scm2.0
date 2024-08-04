dafc2df8580f6f19b4e3012524d519fbbe867de4
aditya shahi <shahi.aditya@gmail.com>
2024-08-04 12:52:29 -0400

    added signup page

--- src/main/java/com/scm/controller/HomePage.java
+++ src/main/java/com/scm/controller/HomePage.java
@@ -2,7 +2,11 @@ package com.scm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.scm.forms.UserForm;
 
 @Controller
 public class HomePage {
@@ -11,13 +15,11 @@ public class HomePage {
     public String Home(Model model) {
         model.addAttribute("pageTitle", "home");
         model.addAttribute("name", "Smart Contact Manager");
-        System.out.println("welcome to the home page");
         return "Home";
     }
 
     @RequestMapping("/about")
     public String about(Model model) {
-        System.out.println("About page");
         model.addAttribute("pageTitle", "About Us");
         return "about";
     }
@@ -25,22 +27,32 @@ public class HomePage {
     @RequestMapping("/services")
     public String services(Model model) {
         model.addAttribute("pageTitle", "Services");
-        System.out.println("Services page");
         return "services";
     }
 
     @RequestMapping("/signup")
     public String signup(Model model) {
+        UserForm userForm = new UserForm();
+        model.addAttribute("userForm", userForm);
         model.addAttribute("pageTitle", "signup");
-        System.out.println("Signup page");
         return "register";
     }
 
     @RequestMapping("/login")
     public String login(Model model) {
         model.addAttribute("pageTitle", "signup");
-        System.out.println("Login page");
         return "login";
     }
 
+    @RequestMapping(value = "/do-register", method = RequestMethod.POST)
+    public String processRegister(@ModelAttribute UserForm userForm) {
+        System.out.println("processing started for the form data");
+        // fetch form data.
+        // validate form data
+        System.out.println("Submit the form: " + userForm.toString());
+        // save to the database
+        // message = that successfull regration completed
+        // redirect to signup page.
+        return "redirect:signup";
+    }
 }
