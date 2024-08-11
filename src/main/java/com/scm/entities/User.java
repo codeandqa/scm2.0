e7a9f04b4025eaf1c8029561a22e86cde0ce8600
aditya shahi <shahi.aditya@gmail.com>
2024-08-11 17:10:58 -0400

    completed the role list

--- src/main/java/com/scm/entities/User.java
+++ src/main/java/com/scm/entities/User.java
@@ -37,26 +37,34 @@ public class User implements UserDetails {
     @Column(name = "user_name", nullable = false)
 
     private String name;
+
     @Column(unique = true, nullable = false)
     private String email;
+
     @Getter(AccessLevel.NONE)
     private String password;
+
     @Column(length = 1000)
     private String about;
+
     @Column(length = 1000)
     private String profilePic;
+
     private String phoneNumber;
 
     @Getter(value = AccessLevel.NONE)
+
     // information
     private boolean enabled = false;
 
     private boolean emailVerified = false;
+
     private boolean phoneVerified = false;
 
     @Enumerated(value = EnumType.STRING)
     // SELF, GOOGLE, FACEBOOK, TWITTER, LINKEDIN, GITHUB
     private Providers provider = Providers.SELF;
+
     private String providerUserId;
 
     // add more fields if needed
@@ -66,11 +74,8 @@ public class User implements UserDetails {
     @ElementCollection(fetch = FetchType.EAGER)
     private List<String> roleList = new ArrayList<>();
 
-    private String emailToken;
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        // list of roles[USER,ADMIN]
         // Collection of SimpGrantedAuthority[roles{ADMIN,USER}]
         Collection<SimpleGrantedAuthority> roles = roleList.stream().map(role -> new SimpleGrantedAuthority(role))
                 .collect(Collectors.toList());
